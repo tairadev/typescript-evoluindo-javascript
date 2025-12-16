fd9b8b3ac1954982e25107835213f430cf75062d
Guilherme Henrique Taira <53182990+tairadev@users.noreply.github.com>
2025-12-15 22:48:44 -0300

    Final da aula 3

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,41 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacaoController {
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
+
+  constructor() {
+    this.inputData = document.querySelector('#data') as HTMLInputElement;
+    this.inputQuantidade = document.querySelector(
+      '#quantidade'
+    ) as HTMLInputElement;
+    this.inputValor = document.querySelector('#valor') as HTMLInputElement;
+  }
+
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    console.log(negociacao);
+    this.limparFormulario();
+  }
+
+  criaNegociacao(): Negociacao {
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ','));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+
+    return new Negociacao(
+      date,
+      quantidade,
+      valor
+    )
+  }
+
+  limparFormulario(): void {
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
+    this.inputData.focus();
+  }
+}
